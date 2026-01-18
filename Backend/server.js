27cc78229d4a9d903e2c28002fb53bc4092aa8ed
KimSuMin123 <tntn211@naver.com>
2026-01-18 16:53:34 +0900

    디비 모드

--- Backend/server.js
+++ Backend/server.js
@@ -65,7 +65,7 @@ app.use('/api/data-repairs',DataRepairRouter)
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // 데이터베이스 연결 및 서버 실행
-sequelize.sync({force:true}) // force: false는 기존 테이블을 삭제하지 않고 유지함
+sequelize.sync({alter: true}) // force: false는 기존 테이블을 삭제하지 않고 유지함
   .then(() => {
     console.log('✅ 데이터베이스 연결 성공 및 테이블 동기화 완료!');
     
