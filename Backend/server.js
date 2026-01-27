ae17b3faf70c3a807a8e5043c5a0cd1b890e582f
KimSuMin123 <tntn211@naver.com>
2026-01-27 22:42:19 +0900

    force: false는 기존 테이블을 삭제하지 않고 유지함

--- Backend/server.js
+++ Backend/server.js
@@ -66,7 +66,7 @@ app.use('/api/data-repairs',DataRepairRouter)
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/qna', qnaRouter);
 // 데이터베이스 연결 및 서버 실행
-sequelize.sync({force: true}) // force: false는 기존 테이블을 삭제하지 않고 유지함
+sequelize.sync({force: false}) // force: false는 기존 테이블을 삭제하지 않고 유지함
   .then(() => {
     console.log('✅ 데이터베이스 연결 성공 및 테이블 동기화 완료!');
     
